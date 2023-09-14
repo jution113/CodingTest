b0e3a8df41ffaa07ea03f44d76b840b2df45305c
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2023-09-14 11:01:19 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -5,48 +5,43 @@
 
 using namespace std;
 
-bool isFind;
+int maxCase = 7;
 
-void combination(int startIdx, vector<int> srcV, vector<int>& resultV) { // 시작 인덱스, 소스 vector
-    // 9c7의 합이 100이라면 출력
-    if(isFind) return;
+// 7 난쟁이 키를 담을 백터
+vector<int> result;
 
-    if(resultV.size() == 7) {
-        // 현재 조합의 합이 100이라면 종료 조건을 활성화 시킨다.
-        int sum = accumulate(resultV.begin(), resultV.end(), 0);
-        if(sum == 100) isFind = true;
+bool isFindResult;
+
+void combi(int startIdx, vector<int> src) {
+    if(result.size() == maxCase) {
+        if(accumulate(result.begin(), result.end(), 0) == 100) {
+            isFindResult = true;
+        }
         return;
     }
 
-    for(int i = startIdx + 1; i < 9; i++) {
-        resultV.push_back(srcV[i]);
-        combination(i, srcV, resultV);
-        if(isFind) return;
-        resultV.pop_back();
+    for(int i = startIdx + 1; i < src.size(); i++) {
+        result.push_back(src[i]);
+        combi(i, src);
+        if(isFindResult) return;
+        result.pop_back();
     }
 }
 
 int main() {
-    // 9 난쟁이를 담을 백터
-    vector<int> src;
+    // 9 난쟁이 키 입력 후 오름차순 정렬
+    vector<int> inputs;
 
-    // 7 난쟁이를 담을 백터
-    vector<int> result;
-
-    // 9번 입력 받아 src에 저장
     for(int i = 0; i < 9; i++) {
         int input;
         cin >> input;
-        src.push_back(input);
+        inputs.push_back(input);
     }
 
-    // 9 난쟁이 오름차순 정렬
-    sort(src.begin(), src.end());
+    sort(inputs.begin(), inputs.end());
 
-    // 조합 시작
-    combination(-1, src, result);
+    combi(-1, inputs);
 
-    // 최종 결과 출력
     for(int i : result) {
         cout << i << "\n";
     }
