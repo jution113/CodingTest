8b051fee932476940713421b55e494b89f7f619b
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2024-09-03 16:56:44 +0900

    [level 2] Title: 주식가격, Time: 64.18 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,12 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
+        Stack<Integer> stack = new Stack<> ();
+        
         for(int i = 0; i < prices.length; i++) {
-            for(int j = i + 1; j < prices.length; j++) {
-                answer[i]++;
-                if(prices[i] > prices[j]) break;
+            if(stack.isEmpty()) {
+                stack.push(i);
+                continue;
             }
+            
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int j = stack.pop();
+                answer[j] = i - j;
+            }
+            
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()) {
+            int i = stack.pop();
+            answer[i] = prices.length - 1 - i;
         }
         
         return answer;
