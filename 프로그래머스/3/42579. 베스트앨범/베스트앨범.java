624ba3492ca67b5d084afca6619a248ca5adb0ae
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2024-09-11 19:15:50 +0900

    [level 3] Title: 베스트앨범, Time: 16.21 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,67 +1,42 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        // 장르별 조회수를 담을 맵
-        Map<String, Integer> genreWithPlayMap = new HashMap<> ();
-        // 음악별 정보를 담을 리스트
-        List<SongInfo> songInfoList = new ArrayList<> ();
-        
+        // 두번째 버전
+        // 장르별 조회수
+        Map<String, Integer> genrePlayInfo = new HashMap<> ();
+        // 장르별 곡의 고유 번호와 조회수
+        Map<String, List<Integer[]>> musicInfoByGenre = new HashMap<> ();
+
         for(int i = 0; i < genres.length; i++) {
             String genre = genres[i];
             int play = plays[i];
-            
-            // 장르별 조회수 저장 및 업데이트
-            if(genreWithPlayMap.containsKey(genre)) {
-                genreWithPlayMap.put(genre, genreWithPlayMap.get(genre) + play);
-            } else {
-                genreWithPlayMap.put(genre, play);
+
+            if(!genrePlayInfo.containsKey(genre)) {
+                genrePlayInfo.put(genre, 0);
+                musicInfoByGenre.put(genre, new ArrayList<> ());
             }
-            
-            // 음악별 정보 저장
-            songInfoList.add(new SongInfo(i, play, genre));
+
+            genrePlayInfo.put(genre, genrePlayInfo.get(genre) + play);
+            musicInfoByGenre.get(genre).add(new Integer[] {i, play});
         }
-        
-        // 장르별 조회수 내림차순 정렬
-        String[] genreSortedByPlay = genreWithPlayMap.entrySet().stream()
-            .sorted((g1, g2) -> Integer.compare(g2.getValue(), g1.getValue()))
-            .map(g -> g.getKey())
-            .toArray(size -> new String[size]);
-        
-        // 음악별 아이디를 담을 리스트
+
+        List<String> genreSortedByPlay =  genrePlayInfo.entrySet().stream()
+                .sorted((entrySet1, entrySet2) -> Integer.compare(entrySet2.getValue(), entrySet1.getValue()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
         List<Integer> answer = new ArrayList<> ();
-        
-        for(String genre : genreSortedByPlay) {
-            SongInfo[] songInfoArrSortedByPlay = songInfoList.stream()
-                .filter(s -> s.genre.equals(genre))
-                .sorted((s1, s2) -> {
-                    if(s1.play == s2.play) return Integer.compare(s1.id, s2.id);
-                    return Integer.compare(s2.play, s1.play);
-                })
-                .toArray(size -> new SongInfo[size]);
-            
-            // 장르별 조회수가 가장 높은 음악 리스트에 추가
-            answer.add(songInfoArrSortedByPlay[0].id);
-            
-            // 차순위 조회수의 음악이 존재한다면, 장르별 차순위 조회수 음악 리스트에 추가
-            if(songInfoArrSortedByPlay.length >= 2) answer.add(songInfoArrSortedByPlay[1].id);
-        }
-        
-        // 리스트를 배열로 변환하여 반환
-        return answer.stream()
-            .mapToInt(i -> i.intValue())
-            .toArray();
-    }
-    
-    public static class SongInfo {
-        int id;
-        int play;
-        String genre;
-        
-        public SongInfo(int id, int play, String genre) {
-            this.id = id;
-            this.play = play;
-            this.genre = genre;
-        }
+
+        genreSortedByPlay.stream()
+                .forEach(genre -> {
+                    musicInfoByGenre.get(genre).stream()
+                            .sorted((musicInfo1, musicInfo2) -> Integer.compare(musicInfo2[1], musicInfo1[1]))
+                            .limit(2)
+                            .forEach(music -> answer.add(music[0]));
+                });
+
+        return answer.stream().mapToInt(i -> i.intValue()).toArray();
     }
 }
