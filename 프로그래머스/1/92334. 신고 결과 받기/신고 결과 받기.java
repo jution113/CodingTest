88182f593afb968dd5352f4b484f3ed0b43971c4
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2024-09-13 13:26:30 +0900

    [level 1] Title: 신고 결과 받기, Time: 242.71 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,53 +1,37 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        // <신고자 id, 신고 당한 id Set>
+        // Map<신고 당한 자, Set<신고자>>
         Map<String, Set<String>> hashMap = new HashMap<> ();
-        
-        for(String r : report) {
-            String id1 = r.split(" ")[0];
-            String id2 = r.split(" ")[1];
-            
-            if(!hashMap.containsKey(id1)) hashMap.put(id1, new HashSet<String> ());
-            
-            hashMap.get(id1).add(id2); // put을 안해줘도 되나?
-        }
-        
-        // <신고 당한 id : 신고 당한 횟수>
+        // Map<신고자, 메일 받을 횟수>
         Map<String, Integer> hashMap2 = new HashMap<> ();
         
-        // 신고 당한 id마다 신고 횟수 갱신
-        hashMap.entrySet().stream()
-            .forEach(entrySet -> { // 신고 당한 id set
-                entrySet.getValue().stream()
-                    .forEach(id -> { // 신고 당한 id
-                        if(!hashMap2.containsKey(id)) hashMap2.put(id, 0);
-                        hashMap2.put(id, hashMap2.get(id) + 1);
-                    });
-            });
-        
-        // 신고 당한 횟수가 k 이상인 <신고 당한 id> Set
-        Set<String> id2Set = hashMap2.entrySet().stream()
-            .filter(entrySet -> entrySet.getValue() >= k)
-            .map(entrySet -> entrySet.getKey())
-            .collect(Collectors.toSet());
-        
-        int[] answer = new int[id_list.length];
-        
-        for(int i = 0; i < id_list.length; i++) {
-            String id1 = id_list[i];
+        for(String r : report) {
+            String reportingUser = r.split(" ")[0];
+            String reportedUser = r.split(" ")[1];
             
-            if(!hashMap.containsKey(id1)) continue;
+            if(!hashMap.containsKey(reportedUser)) hashMap.put(reportedUser, new HashSet<> ());
+            hashMap.get(reportedUser).add(reportingUser);
             
-            int reportCount = (int) hashMap.get(id1).stream()
-                .filter(id2 -> id2Set.contains(id2))
-                .count();
-            
-            answer[i] = reportCount;
+            hashMap2.put(reportingUser, 0);
         }
         
+        int n = id_list.length;
+        int[] answer = new int[n];
+        
+        hashMap.entrySet().stream()
+            // 신고 당한 횟수가 k 이상인 Set<신고자>을 필터링
+            .filter(entrySet -> entrySet.getValue().size() >= k)
+            .flatMap(entrySet -> entrySet.getValue().stream())
+            .forEach(reportingUser -> {
+                hashMap2.put(reportingUser, hashMap2.get(reportingUser) + 1);
+            });
+        
+        for(int i = 0; i < n; i++) {
+            String user = id_list[i];
+            if(hashMap2.containsKey(user)) answer[i] = hashMap2.get(user);
+        }
         
         return answer;
     }
