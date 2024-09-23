5ddbe3f60c84412df97dec7823090673a377cdb9
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2024-09-23 16:26:55 +0900

    [D1] Title: 스탬프 찍기, Time: 100 ms, Memory: 18,640 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T= Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i <T; i++) {
+            sb.append("#");
+        }
+        
+        System.out.println(sb.toString());
+	}
+}
