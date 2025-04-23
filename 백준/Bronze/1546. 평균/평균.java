83a6a57182d3c974d352c758b312b78bf6489df3
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2025-04-23 14:51:31 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,31 +1,23 @@
 import java.util.*;
+import java.io.*;
 
-class Main {
-    public static void main(String args[]) {
-        // 1. 과목의 수를 담을 int형 변수 nums를 선언 후 담는다.
-        // 2. 과목의 점수를 담을 배열형 변수 subjectScore[]를 선언한다.
-        // 3. nums만큼 반복하며 시험 점수를 subjectScore[]에 담는다.
-        // 4. 최댓값을 담을 변수 double형 변수 max를 선언한 뒤 subjectScore[]에 값을 담을 때 값을 비교하여 max에 최댓값을 저장한다.
-        // 5. 과목의 총합을 담을 double형 변수 sum을 선언한다.
-        // 6. nums만큼 반복하며 subjectScore[]의 원소 / max * 100의 값을 sum에 더한다.
-        // 7. sum / nums의 값을 반환한다.
-        
-        Scanner sc = new Scanner(System.in);
-        int numsOfSubject = sc.nextInt();
-        double max = 0;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] nums = new int[N];
+        int maxNum = 0;
+        for (int i = 0; i < N; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                nums[i] = num;
+                maxNum = Math.max(maxNum, num);
+        }
         double sum = 0;
-        int[] subjectScores = new int[numsOfSubject];
-            
-        for(int i = 0; i < numsOfSubject; i++) {
-            int score = sc.nextInt();
-            subjectScores[i] = score;
-            max = max < score ? score : max;
+        for (int i = 0; i < N; i++) {
+            sum += (double) nums[i] / maxNum * 100;
         }
-        
-        for(int score : subjectScores) {
-            sum += score / max * 100;
-        }
-        
-        System.out.println(sum / numsOfSubject);
+        System.out.print(sum / N);
     }
 }
