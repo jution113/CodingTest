ac2fa6127689e372b131d961fa116a29f02e5860
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2025-05-22 21:21:10 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 118 ms, Memory: 36,048 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -2,61 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int y = 0;
-	static int x = 0;
-	static List<ArrayList<Integer>> ladder;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for(int tc = 1; tc <= 10; tc++) {
-			br.readLine();
-			boolean testInProgress = true;
-			ladder = new ArrayList<ArrayList<Integer>> ();
-			
-			y = 0;
-			x = 0;
-			
-			// 사다리 생성
-			while(testInProgress) {
-				ladder.add(new ArrayList<> ());
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				int i = 0;
-				
-				while(st.hasMoreTokens()) {
-					int num = Integer.parseInt(st.nextToken());
-					ladder.get(ladder.size() - 1).add(num);
-					if(num == 2) {
-						testInProgress = false;
-						y = ladder.size() - 1;
-						x = i;
-					}
-					i++;
-				}
-			}
-			
-			int w = ladder.get(0).size();
-			
-			// 탐색
-			while(y > 0) {
-				if(x + 1 < w && ladder.get(y).get(x + 1) == 1) {
-					move(1, w);
-				} else if(0 <= x - 1 && ladder.get(y).get(x - 1) == 1) {
-					move(-1, w);
-				} else {
-					y--;
-				}
-			}
-			
-			System.out.println("#" + tc + " " + x);
-		}
-	}
-	
-	static void move(int dir, int w) {
-		while(0 <= x + dir && x + dir < w && ladder.get(y).get(x + dir) == 1) {
-			x += dir;
-		}
-		y--;
-	}
+    static int[][] map = new int[100][100];
+    static int y;
+    static int x;
+    static int[] dirY = {0, 0, -1};
+    static int[] dirX = {1, -1, 0};
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        for (int t = 1; t <= 10; t++) {
+            int tc = Integer.parseInt(br.readLine());
+            sb.append("#").append(tc).append(" ");
+            
+            for (int i = 0; i < 100; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 100; j++) {
+                    int n = Integer.parseInt(st.nextToken());
+                    if (n == 2) {
+                        y = i;
+                        x = j;
+                    }
+                    map[i][j] = n;
+                }
+            }
+
+            while (y > 0) {
+                for (int i = 0; i < 3; i++) {
+                    int nextY = y + dirY[i];
+                    int nextX = x + dirX[i];
+                    if (nextX >= 0 && nextX < 100 && map[nextY][nextX] == 1) {
+                        map[y][x] = 0;
+                        y = nextY;
+                        x = nextX;
+                        continue;
+                    }
+                }
+            }
+            sb.append(x).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
