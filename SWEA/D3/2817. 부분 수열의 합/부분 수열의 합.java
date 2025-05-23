bc55672ffbc1f79f384ba730755334390b91d102
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2025-05-23 20:48:21 +0900

    [D3] Title: 부분 수열의 합, Time: 287 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -1,44 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Solution
-{
-    static int N;
-    static int K;
-    static List<Integer> arr;
-    static int result;
-    
-	public static void main(String args[]) throws IOException
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int TC = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        
-        for(int tc = 1; tc <= TC; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine());
-            arr = new ArrayList<> ();
-            while(st.hasMoreTokens()) arr.add(Integer.parseInt(st.nextToken()));
-            
-            result = 0;
-            for(int i = 1; i <= N; i++) makeCombination(0, 0, i, 0);
-
-            sb.append(String.format("#%d %d\n", tc, result));
-        }
-        System.out.println(sb.toString());
-    }
-    
-    static void makeCombination(int start, int curDepth, int maxDepth, int sum) {
-        if(curDepth == maxDepth) {
-            if(sum == K) result++;
-            return;
-        }
-        
-        for(int i = start; i < N; i++) {
-            makeCombination(i + 1, curDepth + 1, maxDepth, sum + arr.get(i));
-        }
-    }
+public class Solution {
+	static int K;
+	static int N;
+	static int[] arr;
+	static int cnt;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int TC = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= TC; tc++) {
+			sb.append("#").append(tc).append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			arr = new int[N];
+			cnt = 0;
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+			for (int i = 0; i < N; i++) comb(0, 0, 0, i);
+			sb.append(cnt).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static void comb(int start, int sum, int peekCnt, int maxPeekCnt) {
+		if (sum > K) {
+			return ;
+		}
+		if (peekCnt == maxPeekCnt) {
+			if (sum == K) cnt++;
+			return ;
+		}
+		
+		for (int i = start; i < N; i++) {
+			comb(i + 1, sum + arr[i], peekCnt + 1, maxPeekCnt);
+		}
+	}
 }
