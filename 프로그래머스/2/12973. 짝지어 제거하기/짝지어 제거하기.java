7991eed0dfbe1b174884d9b5f83dbf445f2c4830
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-01-01 18:01:46 +0900

    [level 2] Title: 짝지어 제거하기, Time: 64.97 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,25 +1,23 @@
+import java.io.*;
 import java.util.*;
 
 class Solution
 {
     public int solution(String s)
     {
-        Stack<Character> stack = new Stack<> ();
+        Stack<Integer> st = new Stack<> ();
+        st.push(s.charAt(0) - '0');
         
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for (int i = 1; i < s.length(); i++) {
+            int c = s.charAt(i) - '0';
             
-            if(stack.empty()) {
-                stack.push(c);
+            if (!st.empty() && st.peek() == c) {
+                st.pop();
             } else {
-                if(stack.peek() == c) {
-                    stack.pop();
-                } else {
-                    stack.push(c);
-                }
+                st.push(c);
             }
         }
-        
-        return stack.size() == 0 ? 1: 0;
+
+        return st.empty() ? 1 : 0;
     }
 }
