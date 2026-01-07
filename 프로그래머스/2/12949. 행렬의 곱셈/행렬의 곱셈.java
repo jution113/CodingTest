09cb7d2db3fed7c20c54867820b8cd1c3ff150eb
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-01-07 12:24:37 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.44 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -2,16 +2,16 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int rowLen = arr1.length;
         int colLen = arr2[0].length;
+        int comLen = arr1[0].length;
         int[][] answer = new int[rowLen][colLen];
-        
+
         for (int row = 0; row < rowLen; row++) {
             for (int col = 0; col < colLen; col++) {
-                for (int comLen = 0; comLen < arr1[0].length; comLen++) {
-                    answer[row][col] += arr1[row][comLen] * arr2[comLen][col];
+                for (int com = 0; com < comLen; com++) {
+                    answer[row][col] += arr1[row][com] * arr2[com][col];
                 }
             }
         }
-        
         return answer;
     }
 }
