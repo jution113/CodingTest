d829762caba5750d5f20709459568069db675d85
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-01-08 16:35:47 +0900

    [level 2] Title: 기능개발, Time: 2.70 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,45 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new ArrayDeque<> ();
-        
-        for(int progress : progresses) {
-            queue.offer(progress);
-        }
-        
-        int bendCount = 1;
-        int bendStandard = getNeedDay(progresses[0] , speeds[0]);
-        
         ArrayList<Integer> answer = new ArrayList<> ();
-        
-        for(int i = 1; i < progresses.length; i++) {
-            queue.poll();
-            int needDay = getNeedDay(progresses[i] , speeds[i]);
-            
-            if(needDay <= bendStandard) {
-                bendCount++;
-            } else {
-                answer.add(bendCount);
-                bendCount = 1;
-                bendStandard = needDay;
-            }
-        }
-        
-        answer.add(bendCount);
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-    
-    static private int getNeedDay(int progress, int speed) {
+        int i = 0;
         int day = 1;
-        int remain = 100 - progress;
+        int curDeployCnt = 0;
+        int preDeployCnt = 0;
         
-        if(remain <= speed) return day;
+        while (i < progresses.length) {
+            if (progresses[i] + day * speeds[i] >= 100) {
+                curDeployCnt++;
+                i++;
+            } else {
+                day++;
+                curDeployCnt = 0;
+                if (curDeployCnt != preDeployCnt) {
+                    answer.add(preDeployCnt);
+                }
+            }
+            preDeployCnt = curDeployCnt;
+        }
+        answer.add(curDeployCnt);
         
-        day = remain / speed;
         
-        if(remain % speed > 0) day++;
-        
-        return day;
+        return answer.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
