c69f01052235f2ff28e906fe428bcf1eaf17115d
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-01-22 19:38:47 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.76 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        if (n <= 3)
+            return n;
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        
+        return dp[n];
+    }
+}
