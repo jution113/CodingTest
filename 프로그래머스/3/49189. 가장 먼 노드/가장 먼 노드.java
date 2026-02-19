30d1efb50c4a56020d226b56f68651adac231d62
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-02-19 12:59:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 32.26 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    int maxDepth;
+    int maxDepthNodeCnt;
     
     static class Node {
         int num;
@@ -14,7 +14,7 @@ class Solution {
     }
     
     public int solution(int n, int[][] edge) {
-        maxDepth = 0;
+        maxDepthNodeCnt = 1;
         HashMap<Integer, Integer> nodeCntBydepth = new HashMap<> ();
         ArrayList<Integer>[] graph = new ArrayList[n + 1];
         
@@ -27,19 +27,20 @@ class Solution {
             graph[edgeInfo[1]].add(edgeInfo[0]);
         }
         
-        bfs(n, graph, nodeCntBydepth);
+        bfs(n, graph);
         
-        return nodeCntBydepth.get(maxDepth);
+        return maxDepthNodeCnt;
     }
     
-    private void bfs(int n, ArrayList<Integer>[] graph, HashMap<Integer, Integer> nodeCntBydepth) {
+    private void bfs(int n, ArrayList<Integer>[] graph) {
+        
         ArrayDeque<Node> que = new ArrayDeque<> ();
         que.offer(new Node(1, 0));
         
         boolean[] visited = new boolean[n + 1];
         visited[1] = true;
         
-        nodeCntBydepth.put(0, 1);
+        int maxDepth = 0;
         
         while (!que.isEmpty()) {
             Node node = que.poll();
@@ -49,8 +50,13 @@ class Solution {
                     continue;
                 visited[nextNodeNum] = true;
                 que.offer(new Node(nextNodeNum, node.depth + 1));
-                maxDepth = Math.max(maxDepth, node.depth + 1);
-                nodeCntBydepth.put(node.depth + 1, nodeCntBydepth.getOrDefault(node.depth + 1, 0) + 1);
+                
+                if (maxDepth < node.depth + 1) {
+                    maxDepth = node.depth + 1;
+                    maxDepthNodeCnt = 1;
+                } else {
+                    maxDepthNodeCnt++;
+                }
             }
         }
     }
