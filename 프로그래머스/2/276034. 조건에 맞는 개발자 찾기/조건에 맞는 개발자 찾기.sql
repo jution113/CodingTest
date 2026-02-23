779070eaae55dc73c6d48ec5ef69eaa82670f445
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-02-23 18:46:28 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+SELECT DISTINCT d.ID, d.EMAIL, d.FIRST_NAME, d.LAST_NAME
+FROM DEVELOPERS AS d,
+    SKILLCODES AS s
+WHERE (s.NAME = 'Python' AND s.CODE & d.SKILL_CODE > 0)
+    OR (s.NAME = 'C#' AND s.CODE & d.SKILL_CODE > 0)
+ORDER BY d.ID;
