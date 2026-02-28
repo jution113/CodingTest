00e065a231e62b72ed2bb468c9a2aacdd9794d08
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-02-28 22:45:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 620 ms, Memory: 69160 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,68 +1,53 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        final int IDX_OFFSET = 1;
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int nodeCnt = Integer.parseInt(br.readLine());
-
-        // 인접 리스트 생성
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
         
-        for(int i = 0; i <= nodeCnt; i ++) {
-            // 인덱스를 활용하여 1부터 접근하기 위해 0 추가
-            adjList.add(new ArrayList<>());
+        ArrayList<Integer>[] graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<> ();
         }
         
-        for(int i = IDX_OFFSET + 1; i <= nodeCnt; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int input1 = Integer.parseInt(st.nextToken());
-            int input2 = Integer.parseInt(st.nextToken());
-
-            adjList.get(input1).add(input2);
-            adjList.get(input2).add(input1);
-        }
-
-        // 노드의 부모 저장 배열
-        int[] parentsInfo = findParent(nodeCnt, adjList);
-
-        
-        for(int i = IDX_OFFSET + 1; i <= nodeCnt; i++) {
-            sb.append(parentsInfo[i]).append('\n');
-        }
-
-        System.out.print(sb);
-    }
-
-    static int[] findParent(int nodeCnt, ArrayList<ArrayList<Integer>> adjList) {
-        final int IDX_OFFSET = 1;
-        final int DEFAULT_ROOT_VALUE = 1; 
-
-        // 방문 여부 검사 배열
-        boolean[] visited = new boolean[IDX_OFFSET + nodeCnt];
-        // 노드의 부모 저장 배열
-        int[] parentsInfo = new int[IDX_OFFSET + nodeCnt];
-
-        Queue<Integer> que = new LinkedList<>();
-
-        que.add(DEFAULT_ROOT_VALUE);
-        visited[DEFAULT_ROOT_VALUE] = true;
-
-        while(!que.isEmpty()) {
-            int node = que.poll();
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
             
-            for(int child : adjList.get(node)) {
-                if(visited[child]) continue;
-                que.add(child);
-                visited[child] = true;
-                parentsInfo[child] = node;
+            graph[start].add(end);
+            graph[end].add(start);
+        }
+        
+        int[] parents = new int[N + 1];
+        
+        bfs(N, graph, parents);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= N; i++) {
+            sb.append(parents[i] + "\n");
+        }
+        System.out.println(sb.toString());
+    }
+    
+    private static void bfs(int N, ArrayList<Integer>[] graph, int[] parents) {
+        ArrayDeque<Integer> que = new ArrayDeque<> ();
+        que.offer(1);
+
+        boolean[] visited = new boolean[N + 1];
+        
+        while (!que.isEmpty()) {
+            int start = que.poll();
+            
+            for (int end : graph[start]) {
+                if (!visited[end]) {
+                    visited[end] = true;
+                    que.offer(end);
+                    parents[end] = start;
+                }
             }
         }
-
-        return parentsInfo;
     }
 }
