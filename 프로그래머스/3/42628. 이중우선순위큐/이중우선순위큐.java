6ebf7b571aa6f3042e22ae63fba389e60d4e90ec
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-03-31 13:23:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 82.21 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,29 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        TreeMap<Integer, Integer> tm = new TreeMap<> ();
+        ArrayList<Integer> que = new ArrayList<> ();
         
         for (String operation : operations) {
-            int len = operation.length();
+            String[] strArr = operation.split(" ");
             
             if (operation.charAt(0) == 'I') {
-                int key = Integer.parseInt(operation.substring(2, len));
-                tm.put(key, tm.getOrDefault(key, 0) + 1);
-            } else if (tm.size() > 0) {
-                int key = operation.charAt(2) == '-' ? tm.firstKey() : tm.lastKey() ;
-                
-                if (tm.get(key) - 1 == 0)
-                    tm.remove(key);
-                else
-                    tm.put(key, tm.get(key) - 1);
+                que.add(Integer.parseInt(strArr[1]));
+                Collections.sort(que);
+            } else if (que.size() > 0) {
+                if (operation.charAt(2) == '-') {
+                    que.remove(0);
+                } else {
+                    que.remove(que.size() - 1);
+                }
             }
         }
         
-        int[] answer = new int[2];
-        if (tm.size() > 0) {
-            answer[0] = tm.lastKey();
-            answer[1] = tm.firstKey();
+        int[] answer = {0, 0};
+        if (que.size() == 1) {
+            answer[0] = que.get(0);
+            answer[1] = que.get(0);
+        } else if (que.size() > 1) {
+            answer[1] = que.get(0);
+            answer[0] = que.get(que.size() - 1);
         }
+        
         return answer;
     }
 }
