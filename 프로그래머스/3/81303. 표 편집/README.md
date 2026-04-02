312e3ee97c9634a3a122100240220a9d0fecd49c
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-02 13:50:57 +0900

    [level 3] Title: 표 편집, Time: 507.51 ms, Memory: 248 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/README.md
+++ 프로그래머스/3/81303. 표 편집/README.md
@@ -1,10 +1,10 @@
 # [level 3] 표 편집 - 81303 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303?language=java) 
 
 ### 성능 요약
 
-메모리: 187 MB, 시간: 345.38 ms
+메모리: 248 MB, 시간: 507.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 06일 17:36:57
+2026년 04월 02일 13:50:53
 
 ### 문제 설명
 
--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,40 +1,82 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int n, int k, String[] cmd) {
-        Stack<Integer> deletedRows = new Stack<> ();
-        int[] up = new int[n + 2];
-        int[] down = new int[n + 2];
+    private class Node {
+        int num;
+        int prevNum;
+        int nextNum;
         
-        k++;
-        
-        for(int i = 0; i < n + 2; i++) {
-            up[i] = i - 1;
-            down[i] = i + 1;
+        public Node(int num, int prevNum, int nextNum) {
+            this.num = num;
+            this.prevNum = prevNum;
+            this.nextNum = nextNum;
         }
+    }
+    
+    public String solution(int n, int k, String[] cmds) {
+        // Node 관리 맵 초기화
+        HashMap<Integer, Node> nodeByNum = new HashMap<> ();
+
+        nodeByNum.put(-1, new Node(-1, -2, 0));
+        nodeByNum.put(n, new Node(n, n - 1, n + 1));
         
-        for(int i = 0; i < cmd.length; i++) {
-            if(cmd[i].charAt(0) == 'C') {
-                up[down[k]] = up[k];
-                down[up[k]] = down[k];
-                deletedRows.push(k);
-                k = down[k] <= n ? down[k] : up[k];
-            } else if(cmd[i].charAt(0) == 'Z') {
-                int recoverRow = deletedRows.pop();
-                up[down[recoverRow]] = recoverRow;
-                down[up[recoverRow]] = recoverRow;
-            } else {
-                for(int j = 0; j < Integer.valueOf(cmd[i].split(" ")[1]); j++) {
-                    k = cmd[i].charAt(0) == 'U' ? up[k] : down[k];
-                }
-            }
+        for (int num = 0; num < n; num++) {
+            nodeByNum.put(num, new Node(num, num - 1, num + 1));
         }
 
+        // Node 삭제 스택
+        ArrayDeque<Integer> removedNodeStack = new ArrayDeque<> ();
+        
+        // 명령어 실행
+        int curNodeNum = k;
+        
+        for (String cmd : cmds) {
+            String[] cmdInfo = cmd.split(" ");
+            
+            switch (cmdInfo[0]) {
+                case("U"):
+                    int uMove = Integer.parseInt(cmdInfo[1]);
+                    
+                    for (int m = 0; m < uMove; m++) curNodeNum = nodeByNum.get(curNodeNum).prevNum;
+                    break;
+                case("D"):
+                    int dMove = Integer.parseInt(cmdInfo[1]);
+                    
+                    for (int m = 0; m < dMove; m++) curNodeNum = nodeByNum.get(curNodeNum).nextNum;
+                    break;
+                case("C"):
+                    // 삭제 처리
+                    removedNodeStack.push(curNodeNum);
+                    
+                    // 노드 연결 수정
+                    int prevNodeNum = nodeByNum.get(curNodeNum).prevNum;
+                    int nextNodeNum = nodeByNum.get(curNodeNum).nextNum;
+                    
+                    nodeByNum.get(prevNodeNum).nextNum = nextNodeNum;
+                    nodeByNum.get(nextNodeNum).prevNum = prevNodeNum;
+                    
+                    if (nextNodeNum == n) {
+                        curNodeNum = prevNodeNum;
+                    } else {
+                        curNodeNum = nextNodeNum;
+                    }
+                    break;
+                case("Z"):
+                    // 삭제 복구
+                    int recoveryNum = removedNodeStack.pop();
+                    int prevNodeNum2 = nodeByNum.get(recoveryNum).prevNum;
+                    int nextNodeNum2 = nodeByNum.get(recoveryNum).nextNum;
+                    
+                    nodeByNum.get(prevNodeNum2).nextNum = recoveryNum;
+                    nodeByNum.get(nextNodeNum2).prevNum = recoveryNum;
+            }
+        }
+        
         char[] answer = new char[n];
         Arrays.fill(answer, 'O');
         
-        for(Integer row : deletedRows) {
-            answer[row - 1] = 'X';
+        while (!removedNodeStack.isEmpty()) {
+            answer[removedNodeStack.pop()] = 'X';
         }
         
         return new String(answer);
