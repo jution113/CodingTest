4c8735b64df64006e2988d0a6871384830643e61
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-04 18:04:33 +0900

    [level 3] Title: 등굣길, Time: 0.91 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,34 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n + 1][m + 1];
-        
+        int[][] wayMap = new int[n + 1][m + 1];
+        wayMap[1][1] = 1;
         for (int[] puddle : puddles) {
-            int r = puddle[1];
-            int c = puddle[0];
-            dp[r][c] = -1;
+            wayMap[puddle[1]][puddle[0]] = -1;
         }
         
-        for (int r = 1; r <= n; r++) {
-            if (dp[r][1] == -1) break;
-                dp[r][1] = 1;
-        }
-        
-        for (int c = 1; c <= m; c++) {
-            if (dp[1][c] == -1) break;
-                dp[1][c] = 1;
-        }
-        
-        for (int r = 2; r <= n; r++) {
-            for (int c = 2; c <= m; c++) {
-                if (dp[r][c] == -1) continue;
+        for (int y = 1; y <= n; y++) {
+            for (int x = 1; x <= m; x++) {
+                if (y == 1 && x == 1) continue;
+                if (wayMap[y][x] == -1) continue;
                 
-                int left = dp[r][c - 1] == -1 ? 0 : dp[r][c - 1];
-                int up = dp[r - 1][c] == -1 ? 0 : dp[r - 1][c];
-                dp[r][c] = (left + up) % 1000000007;
+                int up = wayMap[y - 1][x] == -1 ? 0 : wayMap[y - 1][x];
+                int right = wayMap[y][x - 1] == -1 ? 0 : wayMap[y][x - 1];
+                
+                wayMap[y][x] = (up + right) %  1000000007;
             }
         }
         
-        int answer = dp[n][m];
-        return answer;
+        return wayMap[n][m];
     }
 }
