497201543cbe7317a3c51ba0ad3fd1fcb47d7e96
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-11 17:19:05 +0900

    [Silver III] Title: N과 M (5), Time: 424 ms, Memory: 47488 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -2,49 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] arr;
-    static boolean[] visited;
-    static List<Integer> peeks = new ArrayList<> ();
-    static StringBuilder sb = new StringBuilder();
-
+    private static int n;
+    private static int m;
+    private static int[] src;
+    private static boolean[] used;
+    private static StringBuilder sb;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        
+        // n개의 수들을 담을 배열 초기화
+        src = new int[n];
         st = new StringTokenizer(br.readLine());
-        arr = new int[N];
-        visited = new boolean[N];
-        for (int i = 0; i < N; i ++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            src[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-        permutation(0);
-        System.out.print(sb);
+        Arrays.sort(src);
+        
+        used = new boolean[n];
+        
+        comb(0, new ArrayList<Integer> ());
+        
+        System.out.println(sb.toString());
     }
-
-    static void permutation(int peekCnt) {
-        if (peekCnt == M) {
-            makeRes();
-            return;
+    
+    private static void comb(int depth, ArrayList<Integer> peeks) {
+        if (depth == m) {
+            for (int peek : peeks) {
+                sb.append(peek + " ");
+            }
+            sb.append("\n");
+            return ;
         }
-
-        for (int i = 0; i < N; i++) {
-            if (visited[i]) continue;
-            visited[i] = true;
-            peeks.add(arr[i]);
-            permutation(peekCnt + 1);
-            visited[i] = false;
+        
+        for (int i = 0; i < n; i++) {
+            if (used[i]) continue;
+            
+            peeks.add(src[i]);
+            used[i] = true;
+            
+            comb(depth + 1, peeks);
+            
             peeks.remove(peeks.size() - 1);
+            used[i] = false;
         }
-    }
-
-    static void makeRes() {
-        for (Integer num : peeks) {
-            sb.append(num).append(" ");
-        }
-        sb.append("\n");
-    }
+    } 
 }
