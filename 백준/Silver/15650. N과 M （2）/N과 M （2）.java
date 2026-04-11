2010a6af503257b0b3aa9cdcc168daae5fc75b10
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-11 17:03:20 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 15764 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,33 +1,36 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
-    static List<Integer> peeks = new ArrayList<>();
-    static StringBuilder sb = new StringBuilder();
-
+    private static int n;
+    private static int m;
+    private static StringBuilder sb;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        comb(1, 0);
-        System.out.print(sb);
+        
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        
+        comb(1, 0, new ArrayList<Integer> ());
+        
+        System.out.println(sb.toString());
     }
-
-    static void comb(int start, int peekCnt) {
-        if (peekCnt == M) {
-            for (Integer num : peeks) {
-                sb.append(num).append(" ");
+    
+    private static void comb(int start, int depth, ArrayList<Integer> peeks) {
+        if (depth == m) {
+            for (int peek : peeks) {
+                sb.append(peek + " ");
             }
             sb.append("\n");
-            return ;
+            return;
         }
-
-        for (int i = start; i <= N; i++) {
+        
+        for (int i = start; i <= n; i++) {
             peeks.add(i);
-            comb(i + 1, peekCnt + 1);
+            comb(i + 1, depth + 1, peeks);
             peeks.remove(peeks.size() - 1);
         }
     }
