df2053af7cffb9888b5821df7c0855f5470d1360
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-13 11:31:33 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 136 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -4,37 +4,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         
-        int[] memo = new int[N];
-        Arrays.fill(memo, 1);
-        int[] reverseMemo = new int[N];
-        Arrays.fill(reverseMemo, 1);
+        int[] maxLenMemo = new int[n];
+        Arrays.fill(maxLenMemo, 1);
+        int[] maxLenMemo2 = new int[n];
+        Arrays.fill(maxLenMemo2, 1);
         
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (A[j] < A[i]) {
-                    memo[i] = Math.max(memo[i], memo[j] + 1);
-                }
+                if (arr[j] < arr[i]) maxLenMemo[i] = Math.max(maxLenMemo[i], maxLenMemo[j] + 1);
+                if (arr[n - 1 - j] < arr[n - 1 - i]) maxLenMemo2[n - 1 - i] = Math.max(maxLenMemo2[n - 1 - i], maxLenMemo2[n - 1 - j] + 1);
             }
         }
         
-        int res = memo[N - 1] + reverseMemo[N - 1] - 1;
-        for (int i = N - 2; i >= 0; i--) {
-            for (int j = N - 1; j > i; j--) {
-                if (A[j] < A[i]) {
-                    reverseMemo[i] = Math.max(reverseMemo[i], reverseMemo[j] + 1);
-                }
-            }
-            res = Math.max(res, memo[i] + reverseMemo[i] - 1);
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            maxLen = Math.max(maxLenMemo[i] + maxLenMemo2[i] - 1, maxLen);
         }
         
-        System.out.println(res);
+        System.out.println(maxLen);
     }
 }
