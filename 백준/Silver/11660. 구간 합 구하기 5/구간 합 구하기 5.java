104addb80c338a1c86e4f29f5fddb5bc83f0615f
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-18 19:19:18 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 808 ms, Memory: 130164 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,34 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static int n;
+    private static int m;
+    private static long[][] memo;
+        
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[N + 1][N + 1];
         
-        for (int y = 1; y <= N; y++) {
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        memo = new long[n + 1][n + 1];
+        
+        for (int y = 1; y <= n; y++) {
             st = new StringTokenizer(br.readLine());
             
-            for (int x = 1; x <= N; x++) {
-                int num = Integer.parseInt(st.nextToken());
-                
-                dp[y][x] = num + dp[y - 1][x] + dp[y][x - 1] - dp[y - 1][x - 1];
+            for (int x = 1; x <= n; x++) {
+                long num = Long.parseLong(st.nextToken());
+                memo[y][x] = memo[y][x - 1] + memo[y - 1][x] - memo[y - 1][x - 1] + num;
             }
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
+        
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x1 = Integer.parseInt(st.nextToken());
+            
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             
-            int res = dp[y2][x2] - (dp[y2][x1 - 1] + dp[y1 - 1][x2] - dp[y1 - 1][x1 - 1]);
+            long res = memo[y2][x2] - (memo[y2][x - 1] + memo[y - 1][x2] - memo[y - 1][x - 1]);
             sb.append(res + "\n");
         }
+        
         System.out.println(sb.toString());
     }
 }
