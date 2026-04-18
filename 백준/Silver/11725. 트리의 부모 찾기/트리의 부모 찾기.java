d5b20f60933f0731e45535bca1ad32c587e784df
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-18 19:48:03 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 620 ms, Memory: 69028 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -2,41 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static int n;
+    private static ArrayList<Integer>[] graph;
+        
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
         
-        ArrayList<Integer>[] graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
+        n = Integer.parseInt(st.nextToken());
+
+        // 그래프 초기화
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<> ();
         }
-        
-        for (int i = 0; i < N - 1; i++) {
+
+        for (int i = 2; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            
-            graph[start].add(end);
-            graph[end].add(start);
+
+            int node = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+
+            graph[node].add(node2);
+            graph[node2].add(node);
         }
         
-        int[] parents = new int[N + 1];
-        
-        dfs(1, 0, graph, parents);
-        
+        int[] parent = new int[n + 1];
+        bfs(1, parent);
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 2; i <= N; i++) {
-            sb.append(parents[i] + "\n");
+
+        for (int i = 2; i <= n; i++) {
+            sb.append(parent[i] + "\n");
         }
+        
         System.out.println(sb.toString());
     }
-    
-    private static void dfs(int start, int parent, ArrayList<Integer>[] graph, int[] parents) {
-        for (int end : graph[start]) {
-            if (end != parent) {
-                parents[end] = start;
-                dfs(end, start, graph, parents);
+
+    private static void bfs(int start, int[] parent) {
+        ArrayDeque<Integer> que = new ArrayDeque<> ();
+        que.offer(start);
+
+        boolean[] visit = new boolean[n + 1];
+        visit[1] = true;
+
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+
+            for (int next : graph[cur]) {
+                if (visit[next])
+                    continue;
+                que.offer(next);
+                visit[next] = true;
+                parent[next] = cur;
             }
         }
     }
