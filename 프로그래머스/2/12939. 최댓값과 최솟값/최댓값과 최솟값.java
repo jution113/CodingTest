61512b6c4da92620af2784d3f1f96eceb7da265c
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-04-25 22:09:23 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.40 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,16 +1,14 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String[] strArr = s.split(" ");
-        int n = strArr.length;
-        int[] intArr = new int[n];
-        for (int i = 0; i < n; i++) {
-            intArr[i] = Integer.parseInt(strArr[i]);
-        }
-        Arrays.sort(intArr);
+        int[] iArr = Arrays.stream(s.split(" "))
+            .mapToInt(Integer::parseInt)
+            .sorted()
+            .toArray();
         
-        String answer = intArr[0] + " " + intArr[n - 1];
+        String answer = iArr[0] + " " + iArr[iArr.length - 1];
         return answer;
     }
 }
