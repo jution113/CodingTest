12c18bbb37f2b7589199d7b00971dd1f7ae6c367
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-05-28 16:22:56 +0900

    [level 3] Title: 양과 늑대, Time: 5.32 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,48 +1,41 @@
 import java.util.*;
 
 class Solution {
+    private int n;
     private int maxSheep;
     private int[] info;
     private ArrayList<Integer>[] graph;
     
     public int solution(int[] info, int[][] edges) {
+        n = info.length;
         this.info = info;
-        graph = new ArrayList[info.length];
-        for (int i = 0; i < info.length; i++) graph[i] = new ArrayList<> ();
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<> ();
         for (int[] edge : edges) graph[edge[0]].add(edge[1]);
         
         maxSheep = 0;
-        bfs(0);
+        
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+        dfs(1, 0, visited);
         
         return maxSheep;
     }
     
-    private void bfs(int start) {
-        // [0] = node, [1] = sheep, [2] = wolf, [3] = visit
-        ArrayDeque<long[]> que = new ArrayDeque<> ();
-        que.offer(new long[] {start, 1, 0, 1});
+    private void dfs(int sheep, int wolf, boolean[] visited) {
+        maxSheep = Math.max(maxSheep, sheep);
         
-        while(!que.isEmpty()) {
-            long[] cur = que.poll();
-            long node = cur[0];
-            long sheep = cur[1];
-            long wolf = cur[2];
-            long visit = cur[3];
-            
-            maxSheep = Math.max(maxSheep, (int) sheep);
-            
-            for (int reVisit = 0; reVisit < info.length; reVisit++) {
-                // 재방문 시,
-                if ((visit & (1 << reVisit)) != 0) {
-                    // 재방문 노드의 자식 순회
-                    for (int reVisitChild : graph[reVisit]) {
-                        // 재방문 노드의 자식도 재방문이면 탐색하지 않음
-                        if ((visit & (1 << reVisitChild)) != 0) continue;
-                        long nextSheep = info[reVisitChild] == 0 ? sheep + 1 : sheep;
-                        long nextWolf = info[reVisitChild] == 1 ? wolf + 1 : wolf;
+        for (int s = 0; s < n; s++) {
+            if (visited[s]) {
+                for (int e : graph[s]) {
+                    if (!visited[e]) {
+                        int nextSheep = sheep + (info[e] == 0 ? 1 : 0);
+                        int nextWolf = wolf + (info[e] == 1 ? 1 : 0);
                         if (nextSheep <= nextWolf) continue;
-                        long nextVisit = visit | (1 << reVisitChild);
-                        que.offer(new long[] {reVisitChild, nextSheep, nextWolf, nextVisit});
+                        
+                        visited[e] = true;
+                        dfs(nextSheep, nextWolf, visited);
+                        visited[e] = false;
                     }
                 }
             }
