1ecc5ffdd54f125c7c18adaa275ba2a92ba652ce
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-07-22 18:51:30 +0900

    [level 2] Title: 후보키, Time: 9.10 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,61 +1,67 @@
 import java.util.*;
 
 class Solution {
-    private int rowLen;
-    private int colLen;
-    private ArrayList<ArrayList<Integer>> keyList;
-    private HashSet<ArrayList<Integer>> uniqueKeySet;
     private String[][] relation;
-
+    private int rLen;
+    private int cLen;
+    private int answer;
+    private HashSet<Integer> candidateSet;
+    
     public int solution(String[][] relation) {
         this.relation = relation;
-        rowLen = relation.length;
-        colLen = relation[0].length;
-        uniqueKeySet = new HashSet<> ();
-
-        int minKey = 1;
-
-        while (minKey <= colLen) {
-            keyList = new ArrayList<> ();
-            comb(0, 0, minKey, new ArrayList<> ());
-            validateKey();
-            minKey++;
-        }
-
-        return uniqueKeySet.size();
+        this.rLen = relation.length;
+        this.cLen = relation[0].length;
+        this.answer = 0;
+        this.candidateSet = new HashSet<> ();
+        
+        for (int c = 0; c < cLen; c++)
+            makeCombination(0, 0, c + 1, new ArrayList<> ());
+        
+        return answer;
     }
-
     
-
-    private void comb(int start, int depth, int maxDepth, ArrayList<Integer> key) {
+    private void makeCombination(int start, int depth, int maxDepth, ArrayList<Integer> combination) {
         if (depth == maxDepth) {
-            keyList.add(new ArrayList<> (key));
-            return;
+            if (uniqueValidate(combination) != -1 && minimumValidate(combination) != -1) {
+                // System.out.println(combination.toString() + " -> " + candidate);
+                answer++;
+            }
+            return ;
         }
-
-        for (int col = start; col < colLen; col++) {
-            key.add(col);
-            comb(col + 1, depth + 1, maxDepth, key);
-            key.remove(key.size() - 1);
+        
+        for (int c = start; c < cLen; c++) {
+            combination.add(c);
+            makeCombination(c + 1, depth + 1, maxDepth, combination);
+            combination.remove(combination.size() - 1);
         }
     }
-
-    private void validateKey() {
-        outer:
-        for (ArrayList<Integer> key : keyList) {
-            HashSet<String> keySet = new HashSet<> (); 
-
-            for (int row = 0; row < rowLen; row++) {
-                StringBuilder sb = new StringBuilder();
-                for (int col : key) sb.append(relation[row][col] + " ");
-                if (keySet.contains(sb.toString())) continue outer;
-                keySet.add(sb.toString());
-            }
-
-            for (ArrayList<Integer> uniqueKey : uniqueKeySet) {
-                if (key.containsAll(uniqueKey)) continue outer;
-            }
-            uniqueKeySet.add(key);
+    
+    private int uniqueValidate(ArrayList<Integer> combination) {
+        HashSet<String> set = new HashSet<> ();
+        StringBuilder sb;
+        for (int r = 0; r < rLen; r++) {
+            sb = new StringBuilder();
+            for (int c : combination) sb.append(relation[r][c] + " ");
+            if (set.contains(sb.toString())) return -1;
+            set.add(sb.toString());
         }
+        return combination.size();
+    }
+    
+    private int makeCandidate(ArrayList<Integer> combination) {
+        int candidate = 0;
+        for (int c : combination)
+            candidate |= (1 << c);
+        return candidate;
+    }
+    
+    private int minimumValidate(ArrayList<Integer> combination) {
+        int cur = makeCandidate(combination);
+        
+        for (int prev : candidateSet)
+            if ((cur & prev) == prev) return -1;
+        candidateSet.add(cur);
+        
+        return cur;
     }
 }
