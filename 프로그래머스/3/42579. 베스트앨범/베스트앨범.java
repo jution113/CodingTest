e73e40faef97c1ed6a0b6ce45a1ae360993f9705
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-09-18 16:51:37 +0900

    [level 3] Title: 베스트앨범, Time: 3.53 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,83 +1,83 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Long> genrePlaySumMap = new HashMap<> ();
-        HashMap<String, PriorityQueue<Music>> musicMap = new HashMap<> ();
+    private int LIMIT = 2;
+    
+    private class Genre {
+        String name;
+        int playSum = 0;
         
+        public Genre(String name, int play) {
+            this.name = name;
+            this.playSum += play;
+        }
+    }
+    
+    private class Music {
+        int num;
+        int play;
+        
+        public Music(int num, int play) {
+            this.num = num;
+            this.play = play;
+        }
+    }
+    
+    public int[] solution(String[] genres, int[] plays) {
+        HashMap<String, Integer> playByGenre = new HashMap<> ();
+        HashMap<String, PriorityQueue<Music>> musicsByGenre = new HashMap<> ();
+                
         for (int i = 0; i < genres.length; i++) {
             String genre = genres[i];
             int play = plays[i];
-            long sum = 0;
-            PriorityQueue<Music> pq = new PriorityQueue<> ();
             
-            if (genrePlaySumMap.containsKey(genre)) {
-                sum = genrePlaySumMap.get(genre);
-                pq = musicMap.get(genre);
-            }
-            sum += play;
-            pq.offer(new Music(i, play));
-            genrePlaySumMap.put(genre, sum);
-            musicMap.put(genre, pq);
-        }
-        
-        PriorityQueue<Genre> genrePq = new PriorityQueue<> ();
-        for (Map.Entry entrySet : genrePlaySumMap.entrySet()) {
-            genrePq.offer(new Genre((String) entrySet.getKey(), (long) entrySet.getValue()));
-        }
-        
-        ArrayList<Integer> answerList = new ArrayList<> ();
-        while (!genrePq.isEmpty()) {
-            Genre genre = genrePq.poll();
-            PriorityQueue<Music> musicPq = musicMap.get(genre.name);
+            playByGenre.put(genre, playByGenre.getOrDefault(genre, 0) + play);
             
-            int pollCnt = 0;
-            while (!musicPq.isEmpty() && pollCnt < 2) {
-                answerList.add(musicPq.poll().idx);
-                pollCnt++;
+            if (!musicsByGenre.containsKey(genre)) {
+                // 재생 횟수가 같을 경우 고유 번호가 낮은 순서로 음악 정렬
+                // 재생 횟수가 높은 순서로 음악 정렬
+                musicsByGenre.put(genre, new PriorityQueue<> ((Music m, Music m2) -> {
+                    if (m.play == m2.play) return Integer.compare(m.num, m2.num);
+                    return Integer.compare(m2.play, m.play);
+                }));
+            }
+            
+            musicsByGenre.get(genre).offer(new Music(i, play));
+        }
+        
+        // 총 재생 횟수가 높은 순서로 장르를 정렬
+        PriorityQueue<Genre> sortedGenresByPlay = new PriorityQueue<> ((Genre g, Genre g2) -> {
+            return Integer.compare(g2.playSum, g.playSum);
+        });
+        
+        for (Map.Entry<String, Integer> entrySet : playByGenre.entrySet()) {
+            sortedGenresByPlay.offer(new Genre(entrySet.getKey(), entrySet.getValue()));
+        }
+        
+        ArrayList<Integer> answer = new ArrayList<> ();
+        
+        while (!sortedGenresByPlay.isEmpty()) {
+            Genre sortedGenre = sortedGenresByPlay.poll();
+            
+            PriorityQueue<Music> musics = musicsByGenre.get(sortedGenre.name);
+            
+            int i = 0;
+            while (i < LIMIT && !musics.isEmpty()) {
+                answer.add(musics.poll().num);
+                i++;
             }
         }
         
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
-        
-        return answer;
+        return listToArray(answer);
     }
     
-    static class Genre implements Comparable<Genre> {
-        String name;
-        long playSum;
-        
-        public Genre(String name, long playSum) {
-            this.name = name;
-            this.playSum = playSum;
+    private int[] listToArray(ArrayList<Integer> arrayList) {
+        int[] array = new int[arrayList.size()];
+        for (int i = 0; i < arrayList.size(); i++) {
+            array[i] = arrayList.get(i);
         }
         
-        @Override
-        public int compareTo(Genre other) {
-            if (other.playSum > this.playSum)
-                return 1;
-            return -1;
-        }
-        
+        return array;
     }
     
-    static class Music implements Comparable<Music> {
-        int idx;
-        int play;
-        
-        public Music(int idx, int play) {
-            this.idx = idx;
-            this.play = play;
-        }
-        
-        @Override
-        public int compareTo(Music other) {
-            if (this.play == other.play)
-                return this.idx - other.idx;
-            return other.play - this.play;
-        }
-    }
 }
