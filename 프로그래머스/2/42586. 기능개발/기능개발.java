fb985895859636a9d5cda9e19c71864fb1dd7af2
SungCheon Ju <53260199+jution113@users.noreply.github.com>
2026-09-19 14:32:30 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,31 +1,51 @@
 import java.util.*;
 
 class Solution {
+    /*
+    * @pram : int[] progresses, 해당 작업의 진행률
+    * @parma : int[] speeds, 일률
+    * @return : int[],  각 배포마다 몇 개의 기능이 배포되는지를 담은 배열
+    * 
+    * 제약 사항 1 : 개발은 병렬 / 배포는 순차
+    * 제약 사항 2 : 배포는 하루 1번(하루의 끝)
+    *
+    * 전제 조건 1 : progresses, speeds의 길이는 100 이하
+    */
     public int[] solution(int[] progresses, int[] speeds) {
         ArrayList<Integer> answer = new ArrayList<> ();
-        int i = 0;
-        int day = 1;
-        int curDeployCnt = 0;
-        int preDeployCnt = 0;
+        ArrayDeque<Integer> orderedProgresses = new ArrayDeque<> ();
+        int order = 0;
+        int day = 0;
         
-        while (i < progresses.length) {
-            if (progresses[i] + day * speeds[i] >= 100) {
-                curDeployCnt++;
-                i++;
-            } else {
-                day++;
-                curDeployCnt = 0;
-                if (curDeployCnt != preDeployCnt) {
-                    answer.add(preDeployCnt);
-                }
-            }
-            preDeployCnt = curDeployCnt;
+        // 작업 순서 부여
+        for (int progress : progresses) {
+            orderedProgresses.offer(progress);
         }
-        answer.add(curDeployCnt);
         
+        while(order < progresses.length) {
+            int finished = 0;
+            
+            day++;
+            
+            while (!orderedProgresses.isEmpty() && orderedProgresses.peek() + (speeds[order] * day) >= 100) {
+                orderedProgresses.poll();
+                order++;
+                finished++;
+            }
+            
+            if (finished > 0) answer.add(finished);
+        }
         
-        return answer.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        return listToArray(answer);
+    }
+    
+    
+    private int[] listToArray(ArrayList<Integer> list) {
+        int[] array = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
     }
 }
